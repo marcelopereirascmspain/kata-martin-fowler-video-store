11fb03a87d9dab22b93b600e06e55cc68336b3f2
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-07-20 13:42:49 +0100

    Init.

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import module from '../src';
+
+describe('...', () => {
+  it('should...', () => {
+    let actual = true;
+    let expected = false;
+
+    expect(actual).toEqual(expected);
+  });
+});
